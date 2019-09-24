daf07135e84a19918db7013f31aac54daf567b4a
Mike Lee <34168446+MikeHLee@users.noreply.github.com>
2019-09-23 21:05:47 -0400

    Add files via upload

--- dashcapctrl.ino
+++ dashcapctrl.ino
@@ -1,29 +1,9 @@
-#include "IRremote.h"
-
 int buttonApin = 13; // declare push button pins
 int buttonBpin = 12;
 int buttonCpin = 11;
 int buttonDpin = 10;
 int buttonEpin = 9;
 
-int receiver = 5; // Signal Pin of IR receiver to Arduino Digital Pin 5
-
-/*-----( Declare objects )-----*/
-IRrecv irrecv(receiver);     // create instance of 'irrecv'
-decode_results results;      // create instance of 'decode_results'
-
-void translateIR(){
-
-  switch(results.value){
-  case 0xFF6897: Serial.println("0");    break;
-  case 0xFF30CF: Serial.println("1");    break;
-  case 0xFF18E7: Serial.println("2");    break;
-  case 0xFF7A85: Serial.println("3");    break;
-  case 0xFF10EF: Serial.println("4");    break;
-  case 0xFF38C7: Serial.println("5");    break;
-  }
-
-} //END translateIR
 
 void setup(){
   Serial.begin(9600);
@@ -32,9 +12,6 @@ void setup(){
   pinMode(buttonCpin, INPUT_PULLUP);  
   pinMode(buttonDpin, INPUT_PULLUP);
   pinMode(buttonEpin, INPUT_PULLUP);
-
-  Serial.println("IR Receiver Button Decode"); 
-  irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop(){
@@ -51,11 +28,6 @@ void loop(){
   } else {
     Serial.println(0);
   }
-  
-  if (irrecv.decode(&results)){
-    translateIR(); 
-    irrecv.resume(); // receive the next value
-  }  
-  
+ 
   delay(10);
 }
